b4ee008f72fbbcb69e0ded467bcaa96ed1c78d25
lorraineros <43746757+lorraineros@users.noreply.github.com>
2020-12-08 01:01:35 +0000

    Add files via upload

---
+++ Scripts/SecurityBullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityBullet : MonoBehaviour
+{
+	public float speed = 20f;
+	public int damage = 40;
+	public Rigidbody2D rb;
+	public Transform player;
+	Vector2 moveDirection;
+
+
+	//Skjótta
+	void Start()
+	{
+		this.player = GameObject.FindWithTag("Player").transform;
+		rb = GetComponent<Rigidbody2D>();
+		moveDirection = (player.transform.position - transform.position).normalized * speed;
+		rb.velocity = new Vector2 (moveDirection.x, moveDirection.y);
+		Destroy(gameObject, 0.5f);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Player")
+        {
+			Debug.Log("Hitt");
+			Destroy(gameObject);
+        }
+	}
+
+}
---
+++ Scripts/SecurityShooting.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityShooting : MonoBehaviour
+{
+	public Transform firePoint;
+	public GameObject bulletPrefab;
+	public Transform Player;
+	public int distance = 5;
+	public GameObject[] player;
+
+	float fireRate;
+	float nextFire;
+
+	void Start()
+    {
+		fireRate = 0.5f;
+		nextFire = Time.time;
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+		player = GameObject.FindGameObjectsWithTag("Player");
+		if (Vector2.Distance(Player.position, transform.position) <= distance && (player.Length != 0))
+		{
+			// Ef 
+			if (Time.time > nextFire)
+			{
+				// Skjótta 
+				Shoot();
+				nextFire = Time.time + fireRate;
+			}
+		}
+	}
+
+	void Shoot()
+	{
+		// Spawn kúlu í firepoin
+		Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+	}
+}
